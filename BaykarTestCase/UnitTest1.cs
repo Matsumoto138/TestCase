38d770dcee4873baf83f3b1fac365cccda615480
Mobimenti <139997876+Mobimenti@users.noreply.github.com>
2024-02-24 12:37:04 +0300

    First Commit

---
+++ BaykarTestCase/UnitTest1.cs
@@ -0,0 +1,120 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Collections.ObjectModel;
+
+namespace BaykarTestCase
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NavbarTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://kariyer.baykartech.com/tr/");
+
+            try
+            {
+                
+                var navbarElements = driver.FindElements(By.CssSelector(".nav.navbar-nav.navbar-right > li"));
+
+                
+                foreach (var navbarElement in navbarElements)
+                {
+                    navbarElement.Click();
+                    Thread.Sleep(2000);
+
+                    
+                    var dropdownMenuItems = navbarElement.FindElements(By.CssSelector(".dropdown-menu > li > a"));
+
+                    
+                    foreach (var menuItem in dropdownMenuItems)
+                    {
+                        menuItem.Click();
+                        Thread.Sleep(2000);
+                        
+                        WebDriverWait newPageWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                        newPageWait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+
+                        
+                        if (!IsDropdownMenuDisplayed(navbarElement))
+                        {
+                            driver.Navigate().GoToUrl("https://kariyer.baykartech.com/tr/");
+                            navbarElement.Click();
+                            Thread.Sleep(2000);
+                            dropdownMenuItems = navbarElement.FindElements(By.CssSelector(".dropdown-menu > li > a"));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                // WebDriver'ý kapatma
+                driver.Quit();
+            }
+        }
+
+        [Test]
+        public void LanguageTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://kariyer.baykartech.com/tr/");
+            try
+            {
+                IWebElement langButton = driver.FindElement(By.CssSelector(".siteCustomLi > a"));
+                langButton.Click();
+
+                var url = driver.Url;
+                if (url.Contains("en"))
+                {
+                    Console.WriteLine("Dil deðiþtirme iþlemi baþarýlý");
+                }
+                else
+                {
+                    Console.WriteLine("Dil deðiþtirme iþlemi baþarýsýz");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+
+        [Test]
+        public void CarrierTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+        }
+
+        // Dropdown menünün görünürlüðünü kontrol eden yardýmcý bir metod
+        private bool IsDropdownMenuDisplayed(IWebElement navbarElement)
+        {
+            try
+            {
+                // Dropdown menü öðesini bulma
+                var dropdownMenu = navbarElement.FindElement(By.CssSelector(".dropdown-menu"));
+                return dropdownMenu.Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                // Eðer dropdown menü bulunamazsa veya görüntülenemezse, false döndür
+                return false;
+            }
+        }
+
+    }
+}
